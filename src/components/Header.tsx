fd32f2140095344cacb0a4453c372899adeb9988
Christie Partington <christiedpartington@gmail.com>
2022-05-30 10:25:37 -0700

    Update numbers in header to be monospaced

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,24 +13,22 @@ export default function Header({ elapsedTime, day, time, money }: Props) {
     <>
       <Row className="">
         <Col className="text-right">
-          <h6 className="text-right" style={{ fontFamily: "Monospaced" }}>
-            Elapsed time: {elapsedTime}
+          <h6 className="text-right">
+            Elapsed time: <span className="mono-number">{elapsedTime}</span>
           </h6>
         </Col>
         <Col>
           <h5>Capitalist Clicker Co</h5>
         </Col>
         <Col>
-          <h6 className="text-right" style={{ fontFamily: "Monospaced" }}>
-            Day {day}, {time}
+          <h6 className="text-right">
+            Day {day}: <span className="mono-number">{time}</span>
           </h6>
         </Col>
       </Row>
       <Row className="text-center">
         <Col className="text-center">
-          <h1 className="display-1" style={{ fontFamily: "Monospaced" }}>
-            ${money}
-          </h1>
+          <h1 className="display-1 mono-number">${money}</h1>
         </Col>
       </Row>
     </>
